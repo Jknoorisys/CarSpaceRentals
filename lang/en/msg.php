63409b59480fd0149345a85703e8b15c89cb86dd
Javeriya <javeriya.noorisys@gmail.com>
2023-05-09 15:28:21 +0530

    Manage Car Brands

--- lang/en/msg.php
+++ lang/en/msg.php
@@ -2,15 +2,27 @@
 
 return [
 
+    'Validation Failed!' => 'Validation Failed!',
     'error' => 'Something went wrong, please try again ...',
     'admin' => [
         'get-brands' => [
             'success' => 'Car Brands Fetched Successfully',
             'failure' => 'No Car Brands Found',
         ],
+
+        'get-brand' => [
+            'success' => 'Car Brand Detail Fetched Successfully',
+            'failure' => 'No Such Car Brand Found',
+        ],
+
         'add-brand' => [
-            'success' => 'Car Brands Added/Edited Successfully',
-            'failure' => 'Unable to Add/Edit Car Brand',
+            'success' => 'Car Brands Added Successfully',
+            'failure' => 'Unable to Add Car Brand',
+        ],
+
+        'edit-brand' => [
+            'success' => 'Car Brands Updated Successfully',
+            'failure' => 'Unable to Update Car Brand',
         ],
     ],
 
