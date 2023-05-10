920474d8ae0946766866e33395b040a720dabf2e
Javeriya <javeriya.noorisys@gmail.com>
2023-05-10 10:42:46 +0530

    Make Customer Admin

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\CarBrandsController;
+use App\Http\Controllers\admin\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users\AuthController;
@@ -30,9 +31,15 @@ Route::group(['middleware' => 'jwt.verify'], function () {
 
 // Admin Panel By Javeriya Kauser
 Route::prefix('admin')->group(function () {
+    // Manage Car Brands
    Route::post('get-all-brands' , [CarBrandsController::class, 'getCarBrands']);
    Route::post('add-brand' , [CarBrandsController::class, 'addCarBrand']); 
    Route::post('get-brand' , [CarBrandsController::class, 'getCarBrand']); 
    Route::post('edit-brand' , [CarBrandsController::class, 'editCarBrand']); 
+
+    // Manage Customers
+    Route::post('get-all-customers' , [CustomerController::class, 'getCustomers']);
+    Route::post('change-customers-status' , [CustomerController::class, 'changeCustomerStatus']);
+    Route::post('make-customer-admin' , [CustomerController::class, 'makeCustomerAdmin']);
 
 });
